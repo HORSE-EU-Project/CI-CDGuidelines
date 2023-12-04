ad7f655517a207b83ffede24ebbe008d03389d74
jtamb <juan.tamboleol@um.es>
2023-12-04 13:13:37 +0100

    Add GitHub Actions CI/CD for Java

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class HelloWorldTest {
+	@Test
+	public void testMain() {
+	// Your test code here
+	assertTrue(true); // Example test
+	}
+}
